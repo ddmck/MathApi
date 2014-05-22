98559d14f442fe6b1b11ccd114f929443976097e
Donald McKendrick <ddmckendrick@gmail.com>
2014-05-22 19:08:16 -0400

    Add new relic

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
+require 'newrelic_rpm'
 require_relative 'lib/general'
 require_relative 'lib/addition'
 require_relative 'lib/subtraction'
