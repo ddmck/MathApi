2e906f2e78b7417ffdaabccdff4a9c653f8db4c5
Donald McKendrick <ddmckendrick@gmail.com>
2014-06-03 09:06:04 +0100

    Class/Object structure rather than pseudo functional for addition

--- lib/addition.rb
+++ lib/addition.rb
@@ -1,3 +1,34 @@
+class Addition
+  attr_accessor :num1, :num2, :answer
+
+  def initialize(opts = {})
+    total = setTotal(opts[:max])
+    self.num1 = opts[:num1] || rand(total)
+    self.num2 = opts[:num2] || total - self.num1
+    self.answer = opts[:answer]
+  end
+
+  def correct?
+    if self.num1.to_i + self.num2.to_i == self.answer.to_i
+      true
+    else
+      false
+    end
+  end
+
+  def setTotal(value = 10)
+    total = rand(value.to_i - 1) + 1
+  end
+
+  def to_h
+    { num1: self.num1, num2: self.num2, answer: self.answer }
+  end
+
+  def to_json
+    self.to_h.to_json
+  end
+end
+
 def additionGenerator(opts = {})
   total = rand(opts[:max].to_i)
   num1 = rand(total)
@@ -19,4 +50,12 @@ end
 
 post '/additionchecker.json' do
   additionChecker(params)
+end
+
+post '/additionnew.json' do
+  Addition.new(params).to_json
+end
+
+post '/additioncheckernew.json' do
+  {correct: Addition.new(params).correct?}.to_json
 end
